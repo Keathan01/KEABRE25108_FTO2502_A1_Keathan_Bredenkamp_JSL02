c86b3a8c82fbe6bbcbe55d9cb6de493ca7415a09
Keathan01 <bredenkampk29@gmail.com>
2025-05-04 01:52:54 -0700

    Added  the prompt to add a stus to the task and give an alert if invalid status entered

--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@ function getValidStatus(taskNumber){
 const validStatus = ["todo","doing","done"];
 let status;
 
-
-
+do{
+  status = prompt(`Enter the status for Task ${taskNumber}("todo","doing","done"):`);
+  if (!status || !validStatuses.includes(status.toLowerCase())){
+    alert("invalid status! Please enter only:'todo','doing'or'done'.");
+  }
+} while(!status||!validStatuses.includes(status.toLowerCase()));
+return status.toLowerCase();
 }
