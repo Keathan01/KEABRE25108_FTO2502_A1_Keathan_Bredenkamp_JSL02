d8e72b5ad286c2fd63d43713e58be15a9bd2dc12
Keathan01 <bredenkampk29@gmail.com>
2025-05-04 02:18:11 -0700

    fixed spelling error

--- scripts.js
+++ scripts.js
@@ -12,12 +12,12 @@ do{
 return status.toLowerCase();
 }
 
-const task1 = { title:prompt("Enter the totle for Task 1:"),
+const task1 = { title:prompt("Enter the title for Task 1:"),
 description:prompt("Enter the description for Task 1:"),
 status:getValidStatus(1)
 };
 
-const task2 = { title:prompt("Enter the totle for Task 2:"),
+const task2 = { title:prompt("Enter the title for Task 2:"),
 description:prompt("Enter the description for Task 2:"),
 status:getValidStatus(2)
 };
