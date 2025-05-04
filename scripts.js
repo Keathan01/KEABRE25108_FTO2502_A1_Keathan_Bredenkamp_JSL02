d4db004b9b9ce32bf611b1a34fa8a63604b918cb
Keathan01 <bredenkampk29@gmail.com>
2025-05-04 01:56:51 -0700

    added prompts for task 1 and task 2

--- scripts.js
+++ scripts.js
@@ -11,3 +11,11 @@ do{
 } while(!status||!validStatuses.includes(status.toLowerCase()));
 return status.toLowerCase();
 }
+const task1 = { title:prompt("Enter the totle for Task 1:"),
+description:prompt("Enter the description for Task 1:"),
+status:getValidStatus(1)
+};
+const task2 = { title:prompt("Enter the totle for Task 2:"),
+description:prompt("Enter the description for Task 2:"),
+status:getValidStatus(2)
+};
